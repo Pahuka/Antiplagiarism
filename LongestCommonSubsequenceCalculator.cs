1a53e9424ed5b171b17c765dcdacbdadc153dd97
Pahuka <J-a-c-k@yandex.ru>
2021-07-27 16:15:37 +0500

    Завершил реализацию метода Calculate. Задача сдана.

--- LongestCommonSubsequenceCalculator.cs
+++ LongestCommonSubsequenceCalculator.cs
@@ -13,12 +13,43 @@ namespace Antiplagiarism
 
         private static int[,] CreateOptimizationTable(List<string> first, List<string> second)
         {
-            throw new NotImplementedException();
+            if (first.Count == 0 && second.Count == 0) return new int[0, 0];
+            var result = new int[first.Count + 1, second.Count + 1];
+
+            for (int i = 1; i < first.Count + 1; i++)
+            {
+                for (int j = 1; j < second.Count + 1; j++)
+                {
+                    if (first[i - 1] == second[j - 1]) result[i, j] = result[i - 1, j - 1] + 1;
+                    else result[i, j] = Math.Max(result[i, j - 1], result[i - 1, j]);
+                }
+            }
+            return result;
         }
 
         private static List<string> RestoreAnswer(int[,] opt, List<string> first, List<string> second)
         {
-            throw new NotImplementedException();
+            var result = new List<string>();
+
+            for (int i = first.Count; i > 0;)
+            {
+                for (int j = second.Count; j > 0 && i > 0;)
+                {
+                    if (first[i - 1] == second[j - 1] && opt[i, j] != 0)
+                    {
+                        result.Add(first[i - 1]);
+                        i--;
+                        j--;
+                    }
+
+                    else if (opt[i, j - 1] >= opt[i - 1, j]) j--;
+                    else i--;
+                }
+                break;
+            }
+
+            result.Reverse();
+            return result;
         }
     }
 }
